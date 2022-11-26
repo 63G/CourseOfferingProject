1bf3c7873c1e45725560a84ae357e194ceec1f53
Muath Alsubhi <79430184+63G@users.noreply.github.com>
2022-11-26 15:05:32 +0300

    This is the main file
    Core.java is the main place where are other classes are combined to preform our course offering program. Mainly now, this folder will read CSV files and arrange them into a list of Objects as required. I've created file objects for each file we have in the Data folder

---
+++ Core.java
@@ -0,0 +1,45 @@
+  import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Core {
+    public static void main(String[] args) {
+        File Offering = new File("CourseOffering.csv");
+        File Plan = new File("DegreePlan.csv");
+        File FinishedCourses = new File("FinishedCourses.csv");
+        List<String> list=new ArrayList<String>();
+        List<Course> SectionList = new ArrayList<Course>();
+        OfferingtoObject(Offering, SectionList);
+        // checking method worked
+        System.out.println();
+        System.out.println(SectionList.get(1).getClass());
+
+    }
+
+    // i'll try to make this method general for all files.
+    public static void OfferingtoObject(File fileToRead, List<Course> list){
+        try {
+            Scanner input = new Scanner(fileToRead);
+            input.nextLine(); // this is just a header
+            String line = input.nextLine();
+            System.out.println(line);
+            while(input.hasNext()){
+                String[] SectionInfo = line.split(",");
+                System.out.println(Arrays.toString(SectionInfo));
+                String[] SectionNamenNumber = SectionInfo[0].split("-");
+                System.out.println(Arrays.toString(SectionNamenNumber));
+                list.add(new Section(SectionNamenNumber[0], SectionNamenNumber[1],
+                        SectionInfo[1], Integer.parseInt(SectionInfo[2]), SectionInfo[3], SectionInfo[4], SectionInfo[5], SectionInfo[6], SectionInfo[7], SectionInfo[8]));
+                line = input.nextLine();
+            }
+            System.out.println("DONE");
+            input.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File Isn't found!");
+            throw new RuntimeException(e);
+        }
+    }
+}
