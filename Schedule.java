93b7c47800dae515ba9307a3c526dc8f30412cfa
Abdulrahman <42104950+alwatheq@users.noreply.github.com>
2022-12-17 20:15:59 +0300

    Update Schedule.java

--- Schedule.java
+++ Schedule.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public class Schedule {
     // important:
     // we need to figure out how to avoid time conflict with other courses,
@@ -34,13 +36,46 @@ public class Schedule {
         // * They are on same day but not conflict
         return false;
     }
-public static boolean isRepeat(Course a, Course b){
-    if (((Section) a).getNameWithoutSec().equals(((Section) b).getNameWithoutSec())) {
-        if (((Section)a).getActivity().equals(((Section)b).getActivity())) {
-            return true;
+
+    public static boolean isRepeat(Course a, Course b) {
+        if (((Section) a).getNameWithoutSec().equals(((Section) b).getNameWithoutSec())) {
+            if (((Section) a).getActivity().equals(((Section) b).getActivity())) {
+                return true;
+            }
+            return false;
         }
         return false;
     }
-    return false;
-}
+
+    public static boolean hasLab(Course a) { // ! this function check if couse has lab
+        List<Course> ShowedCourses = Student.AvailableCourses;
+
+        // get course name check all section that have this name and activity lab if it
+        // happend return true; otherwise return false
+        for (int i = 0; i < ShowedCourses.size(); i++) {
+            if (Core.formatName(a.getCourseName()).equals(Core.formatName(ShowedCourses.get(i).getCourseName()))) {
+                if (((Section) ShowedCourses.get(i)).getActivity().equals("LAB")) {
+                    return true;
+                }
+
+            }
+        }
+        return false;
+    }
+
+    public static boolean hasRec(Course a) { // ! this function check if couse has rec
+        List<Course> ShowedCourses = Student.AvailableCourses;
+
+        // get course name check all section that have this name and activity lab if it
+        // happend return true; otherwise return false
+        for (int i = 0; i < ShowedCourses.size(); i++) {
+            if (Core.formatName(a.getCourseName()).equals(Core.formatName(ShowedCourses.get(i).getCourseName()))) {
+                if (((Section) ShowedCourses.get(i)).getActivity().equals("REC")) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
 }
